acc128046d8e9e8410851f1f68b6e2cdbd3baa9a
TheTinyCoder <charawe.yw@gmail.com>
2023-07-25 22:27:48 +0300

    add prototypes for print char & string functions

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+#include <stdarg.h>
+int _putchar(char *c);
+
+int _printf(const char *format, ...);
+
+/* prints a character */
+int print_char(va_list arg);
+/* prints a string */
+int print_str(va_list arg);
+
+#endif
