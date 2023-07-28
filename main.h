e51088848d2b898b931e5c5e0b06dbe5a447725b
TheTinyCoder <charawe.yw@gmail.com>
2023-07-28 07:23:31 +0300

    add util functions prototypes

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+#include <stdarg.h>
 
 
 /**
@@ -10,22 +11,27 @@
 typedef struct printFormat
 {
 	char *format;
-	int (*p)();
+	int (*f)(va_list, char *, unsigned int);
 } print_format;
 
 
-#include <stdarg.h>
 int _putchar(char c);
 
 int _printf(const char *format, ...);
 
 /* prints a character */
-int print_char(va_list arg);
+int print_char(va_list args, char *buf, unsigned int buf_index);
 /* prints a string */
-int print_str(va_list arg);
+int print_str(va_list args, char *buf, unsigned int buf_index);
 
 /* prints an integer */
-int print_int(va_list arg);
-/* prints base 10 */
-int print_dec(va_list arg);
+int print_int(va_list args, char *buf, unsigned int buf_index);
+/* prints an integer in base 10 */
+int print_dec(va_list args);
+
+/* util functions */
+int (*get_format_func(const char *s, int index))(va_list, char *, unsigned int);
+int all_format_functions(const char *s, int index);
+unsigned int handle_buf(char *buf, char c, unsigned int buf_index);
+int print_buf(char *buf, unsigned int buf_index);
 #endif
