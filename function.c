56233a4590f1deccae80c30c73ec958d22665c8d
Henhrie <akinolahenryayobami@gmail.com>
2023-07-28 21:56:08 +0800

    printf

--- function.c
+++ function.c
@@ -1,174 +1,185 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * ...helper functions ...
+ * print_char - Prints a char
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-
-/**
- * _printf - A custom printf function
- */
-int _printf(const char *format, ...)
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-    va_list args;
-    va_start(args, format);
+	char c = va_arg(types, int);
 
-    int char_count = 0;
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
+/**
+ * print_string - Prints a string
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int length = 0, i;
+	char *str = va_arg(types, char *);
 
-            int align_left = 0;
-            int zero_padding = 0;
-            int field_width = 0;
-            int precision = -1;
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
 
-            while (*format == '-' || *format == '0')
-            {
-                if (*format == '-')
-                {
-                    align_left = 1;
-                }
-                else if (*format == '0')
-                {
-                    zero_padding = 1;
-                }
-                format++;
-            }
+	while (str[length] != '\0')
+		length++;
 
-            while (*format >= '0' && *format <= '9')
-            {
-                field_width = field_width * 10 + (*format - '0');
-                format++;
-            }
+	if (precision >= 0 && precision < length)
+		length = precision;
 
-            if (*format == '.')
-            {
-                format++;
-                precision = 0;
-                while (*format >= '0' && *format <= '9')
-                {
-                    precision = precision * 10 + (*format - '0');
-                    format++;
-                }
-            }
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
 
-            switch (*format)
-            {
-                case 'c':
-                {
-                    int c = va_arg(args, int);
-                    if (field_width > 1)
-                    {
-                        char_count += print_with_padding(c, field_width, align_left);
-                    }
-                    else
-                    {
-                        _putchar(c);
-                        char_count++;
-                    }
-                    break;
-                }
-                case 's':
-                {
-                    char *s = va_arg(args, char *);
-                    char_count += print_string(s, field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case '%':
-                {
-                    if (field_width > 1)
-                    {
-                        char_count += print_with_padding('%', field_width, align_left);
-                    }
-                    else
-                    {
-                        _putchar('%');
-                        char_count++;
-                    }
-                    break;
-                }
-                case 'd':
-                case 'i':
-                {
-                    int num = va_arg(args, int);
-                    char_count += print_integer(num, field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case 'u':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    char_count += print_unsigned(num, field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case 'o':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    char_count += print_octal(num, field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case 'x':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    char_count += print_hexadecimal(num, 'x', field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case 'X':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    char_count += print_hexadecimal(num, 'X', field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case 'b':
-                {
-                    unsigned int num = va_arg(args, unsigned int);
-                    char_count += print_binary(num, field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case 'p':
-                {
-                    void *ptr = va_arg(args, void *);
-                    char_count += print_pointer_address(ptr, field_width, precision, align_left);
-                    break;
-                }
-                case 'S':
-                {
-                    char *s = va_arg(args, char *);
-                    char_count += print_custom_string(s, field_width, precision, align_left, zero_padding);
-                    break;
-                }
-                case 'r':
-                {
-                    char *s = va_arg(args, char *);
-                    char_count += print_reversed_string(s, field_width, precision, align_left);
-                    break;
-                }
-                case 'R':
-                {
-                    char *s = va_arg(args, char *);
-                    char_count += print_rot13_string(s, field_width, precision, align_left);
-                    break;
-                }
-                default:
-                {
-                    break;
-                }
-            }
-        }
-        else
-        {
-            _putchar(*format);
-            char_count++;
-        }
+	return (write(1, str, length));
+}
 
-        format++;
-    }
+/**
+ * print_percent - Prints a percent sign
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
 
-    va_end(args);
 
-    return char_count;
+/**
+ * print_binary - Prints an unsigned number
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
