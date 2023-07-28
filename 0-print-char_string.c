b09f9675c00e773248d9716d139591d3b5d46792
TheTinyCoder <charawe.yw@gmail.com>
2023-07-28 07:40:41 +0300

    update char and string functions to use buffers

--- 0-print-char_string.c
+++ 0-print-char_string.c
@@ -1,44 +1,46 @@
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include "main.h"
 
-/**
- * print_char - prints a character c
- * @arg: character to print
- * Return: the no. of characters printed (should be 1)
- */
 
-int print_char(va_list arg)
+/**
+ * print_char - writes the character c to stdout (prints a character)
+ * @args: arguments
+ * @buf: pointer to buffer
+ * @buf_index: index for buffer pointer
+ * Return: 1 (on success)
+ */
+int print_char(va_list args, char *buf, unsigned int buf_index)
 {
 	char c;
 
-	c = va_arg(arg, int);
-
-	_putchar(c);
+	c = va_arg(args, int);
+	handle_buf(buf, c, buf_index);
 
 	return (1);
 }
 
 /**
- * print_str - prints a string s
- * @arg: string to print
- * Return: the no. of characters printed(excluding the null byte)
+ * print_str - writes the string to stdout(prints a string)
+ * @args: arguments
+ * @buf: buffer pointer
+ * @buf_index: index for buffer pointer
+ * Return: 1 (on success)
  */
-
-int print_str(va_list arg)
+int print_str(va_list args, char *buf, unsigned int buf_index)
 {
+	char *str, nil[] = "(null)";
 	unsigned int i;
-	char *a;
 
-	a = va_arg(arg, char *);
-
-	i = 0;
-
-	if (a == NULL)
-		a = "(null)";
-
-	for (; a[i] != '\0'; i++)
-		_putchar(a[i]);
-
+	str = va_arg(args, char *);
+	if (str == NULL)
+	{
+		for (i = 0; nil[i]; i++)
+			buf_index = handle_buf(buf, nil[i], buf_index);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+		buf_index = handle_buf(buf, str[i], buf_index);
 	return (i);
 }
