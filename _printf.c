fd0b72e154e162f67eea37611ae67ade9da66585
TheTinyCoder <charawe.yw@gmail.com>
2023-09-13 19:58:06 +0300

    update _printf loop

--- _printf.c
+++ _printf.c
@@ -9,29 +9,42 @@
 int _printf(const char *format, ...)
 {
 	int i, (*f)(const char *s), len = 0;
-	va_list ptr;
+	va_list args;
 
-	va_start(ptr, format);
-	for (i = 0; format[i]; i++)
+	va_start(args, format);
+	if (format)
 	{
-		if (format[i] == '%' && format[i + 1])
+		for (i = 0; format[i]; i++)
 		{
-			if (format[i + 1] == '%')
+			if (format[i] == '%')
 			{
-				write(1, &format[i + 1], 1), len++, i += 2;
-				continue;
+				i++;
+				if (format[i] == '%')
+				{
+					write(1, &format[i], 1), len++;
+					continue;
+				}
+				else if (format[i] && format[i] != '\n')
+				{
+					f = get_specifier_func(&format[i]);
+					if (f)
+					{
+						len += f(va_arg(args, char *));
+						continue;
+					}
+					else
+					{
+						len += write(1, &format[i - 1], 1);
+						len += write(1, &format[i], 1);
+					}
+				}
 			}
 			else
-				f = get_specifier_func(&format[i + 1]);
-			if (f)
-			{
-				len += f(va_arg(ptr, char *));
-			}
-		}
-		write(1, &format[i], 1), len++;
+				write(1, &format[i], 1), len++;
 
+		}
 	}
 
-	va_end(ptr);
+	va_end(args);
 	return (len);
 }
