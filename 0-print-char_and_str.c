a4b66296b12e4b0b6f9750cbb7f336b235843c0d
TheTinyCoder <charawe.yw@gmail.com>
2023-09-13 19:57:30 +0300

    update functions that handle char and string to deal with null

--- 0-print-char_and_str.c
+++ 0-print-char_and_str.c
@@ -9,12 +9,10 @@
 int print_char(const char *c)
 {
 	if (c)
-	{
-		write(1, &c, 1);
-		return (1);
-	}
+		return (write(1, &c, 1));
 	return (0);
 }
+
 
 /**
  * print_str - writes a string to stdout
@@ -25,12 +23,12 @@ int print_char(const char *c)
 int print_str(const char *s)
 {
 	int i = 0;
-
 	if (s)
 	{
 		for (; s[i]; i++)
 			write(1, &s[i], 1);
 	}
+
 	return (i);
 }
 
