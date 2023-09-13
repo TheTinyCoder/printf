3d8e5339f647d17305fb51dc350fdb18b33efe42
TheTinyCoder <charawe.yw@gmail.com>
2023-09-13 09:02:20 +0300

    add tests for task 0

---
+++ tests/0-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+	int len, len2;
+
+	len = _printf("Let's print a simple sentence.\n");
+	len2 = printf("Let's print a simple sentence.\n");
+	_printf("%c", 'S');
+	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
+	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	_printf("%s", "This sentence is retrieved from va_args!\n");
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	_printf("%%");
+	_printf("Should print a single percent sign: %%\n");
+	_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	_printf("css%ccs%scscscs", 'T', "Test");
+	_printf(NULL);
+	_printf("%");
+	_printf("%!\n");
+	_printf("%K\n");
+
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
+}
