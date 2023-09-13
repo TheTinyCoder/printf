70b0cbace6921ad2f0edae8d85ee948a5cccee81
TheTinyCoder <charawe.yw@gmail.com>
2023-09-13 08:56:17 +0300

    functions to handle char and string

---
+++ 0-print-char_and_str.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_char - writes a character to stdout
+ * @c: pointer to character
+ * Return: 1 on success
+ */
+
+int print_char(const char *c)
+{
+	if (c)
+	{
+		write(1, &c, 1);
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * print_str - writes a string to stdout
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int print_str(const char *s)
+{
+	int i = 0;
+
+	if (s)
+	{
+		for (; s[i]; i++)
+			write(1, &s[i], 1);
+	}
+	return (i);
+}
+
