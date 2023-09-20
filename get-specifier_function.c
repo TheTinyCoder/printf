e5c9ac547c278b717009d2e84b3908451762311d
TheTinyCoder <charawe.yw@gmail.com>
2023-09-20 21:42:55 +0300

    update get function for S specifier

--- get-specifier_function.c
+++ get-specifier_function.c
@@ -14,7 +14,7 @@ int (*get_specifier_func(const char *c))(va_list, char *, int)
 		{"d", print_decimal}, {"i", print_integer},
 		{"b", print_binary}, {"u", print_uint},
 		{"o", print_octal}, {"x", print_hex},
-		{"X", print_hex_upper},
+		{"X", print_hex_upper}, {"S", print_custom_str},
 		{NULL, NULL}
 	};
 
