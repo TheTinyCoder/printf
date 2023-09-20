b3940dc5b8fdb4f44e12ff295f9131948e7f406a
TheTinyCoder <charawe.yw@gmail.com>
2023-09-20 21:46:29 +0300

    write functions to handle custom string S

---
+++ 5-print-custom_string_S.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_custom_str - writes a special string to stdout
+ * @args: va_list
+ * @buf: buffer
+ * @index: buffer index
+ * Return: number of bytes printed
+ */
+
+int print_custom_str(va_list args, char *buf, int index)
+{
+	int i = 0, j, k = 0;
+	char *x = "(null)", *s = va_arg(args, char *), y[2];
+
+	if (s)
+	{
+		for (; s[i]; i++)
+		{
+			j = s[i];
+			if (j >= 32 && j < 127)
+				index = use_buffer(buf, index, s[i]);
+			else
+			{
+				index = use_buffer(buf, index, 92);
+				index = use_buffer(buf, index, 120), k++;
+				y[1] = j % 16 < 10 ? (j % 16) + 48 : ((j % 16) - 10) + 65, j /= 16;
+				y[0] = j % 16 < 10 ? (j % 16) + 48 : ((j % 16) - 10) + 65;
+				index = use_buffer(buf, index, y[0]), k++;
+				index = use_buffer(buf, index, y[1]), k++;
+			}
+		}
+		return (i + k);
+	}
+	for (; x[i]; i++)
+		index = use_buffer(buf, index, x[i]);
+
+	return (6);
+}
+
