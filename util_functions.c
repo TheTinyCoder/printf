e2d3f59287f7c1a30ef14a9512ac675cf71d14d2
TheTinyCoder <charawe.yw@gmail.com>
2023-09-22 12:01:18 +0300

    write function to handle str length

--- util_functions.c
+++ util_functions.c
@@ -77,3 +77,21 @@ int _strcmp(char *s1, const char *s2)
 
 	return (1);
 }
+
+
+/**
+ * _strlen - function entry-point
+ *
+ * Description: returns the length of a string
+ * @s: pointer to array
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+		x++;
+	return (x);
+}
