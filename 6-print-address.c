3cbb7c858a096ae4b2cf1e70d6a8bead20bb49a8
TheTinyCoder <charawe.yw@gmail.com>
2023-10-01 16:33:04 +0300

    refactor and test

--- 6-print-address.c
+++ 6-print-address.c
@@ -11,23 +11,26 @@
 
 int print_address(va_list args, char *buf, int index, identifierPtr ptr)
 {
-	int i = 0, m, n = 0, width;
+	int i = 0, left, m, n = 0, width;
 	unsigned long int j = (uintptr_t)(va_arg(args, void *)), k;
 	char *x = "(nil)", *y;
 
 	if (j)
 	{
+		left = _strchr(ptr->flags, '-') ? 1 : 0;
 		for (k = j; k > 0; k /= 16)
 			i++;
 		width = ptr->width - (i + 2);
-		m = print_flags(buf, index, width, ' ');
-		n += m, index += m;
+		if (left == 0)
+			m = print_flags(buf, index, width, ' '), n += m, index += m;
 		y = malloc(sizeof(char) * (i + 1 + 2));
 		y[i + 2] = '\0';
 		y[0] = 48, y[1] = 120;
 		address_to_hex(j, y, (i + 1));
 		for (k = 0; y[k]; k++)
 			index = use_buffer(buf, index, y[k]);
+		if (left == 1)
+			m = print_flags(buf, index, width, ' '), n += m;
 		free(y);
 		return (i + n + 2);
 	}
